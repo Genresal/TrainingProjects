e39542864290fcd4b3fe75aa7bf7f3c6687b879c
Vadzim Minutka <vadzim_minutka@epam.com>
2022-10-14 21:50:43 +0300

    add rabbit mq test projects

---
+++ RabbitMQConsumer/RabbitMQ/RabbitMqListener.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Extensions.Hosting;
+using System.Text;
+using System.Diagnostics;
+using System;
+
+namespace RabbitMQConsumer.RabbitMQ
+{
+    public class RabbitMqListener : BackgroundService
+    {
+
+        private IConnection connection;
+        private IModel channel;
+
+        public RabbitMqListener()
+        {
+            // Не забудьте вынести значения "localhost" и "MyQueue"
+            // в файл конфигурации
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "MyQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                // Каким-то образом обрабатываем полученное сообщение
+                Debug.WriteLine($"Message received: {content}");
+
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            channel.BasicConsume("MyQueue", false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            channel.Close();
+            connection.Close();
+            base.Dispose();
+        }
+	}
+}
