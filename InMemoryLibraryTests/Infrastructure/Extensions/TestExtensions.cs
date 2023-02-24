8d83820cd0af0147503e58eaa680275cd1f63728
pavel haharyn <pavel.h@bluetape.com>
2023-02-24 16:39:24 +0300

    fix tests

---
+++ InMemoryLibraryTests/Infrastructure/Extensions/TestExtensions.cs
@@ -0,0 +1,40 @@
+﻿using InMemoryCachingLibrary;
+using InMemoryCachingLibrary.Services;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace InMemoryLibraryTests.Infrastructure.Extensions
+{
+	internal static class TestExtensions
+	{
+		private const string ConfigFileName = "appsettings.json";
+		private const string InvalidConfigFileName = "appsettings.Invalid.json";
+
+		public static CacheService CreateValidInstanceOfMemoryCache(Mock<IMemoryCache> memoryCacheMock = default)
+		{
+			return CreateInstanceOfMemoryCache(memoryCacheMock);
+		}
+
+		public static CacheService CreateInvalidInstanceOfMemoryCache(Mock<IMemoryCache> memoryCacheMock = default)
+		{
+			return CreateInstanceOfMemoryCache(memoryCacheMock, InvalidConfigFileName);
+		}
+
+		private static CacheService CreateInstanceOfMemoryCache(Mock<IMemoryCache> memoryCacheMock = default, string configFileName = ConfigFileName)
+		{
+			IMemoryCache memoryCache = memoryCacheMock is not null ? memoryCacheMock.Object : new MemoryCache(new MemoryCacheOptions());
+
+			var configuration = new ConfigurationBuilder()
+					.SetBasePath(Directory.GetCurrentDirectory())
+					.AddJsonFile(configFileName, false)
+					.Build();
+
+			var options = Options.Create(configuration.GetSection(nameof(CacheSettings)).Get<CacheSettings>());
+			var memoryCacheHelper = new CacheService(memoryCache, options);
+
+			return memoryCacheHelper;
+		}
+	}
+}
--- InMemoryLibraryTests/UnitTest1.cs
+++ InMemoryLibraryTests/UnitTest1.cs
@@ -1,97 +1,88 @@
 using FluentAssertions;
-using InMemoryLibraryTests.Infrastructure.Helpers;
+using InMemoryLibraryTests.Infrastructure.Extensions;
 using Microsoft.Extensions.Caching.Memory;
 using Moq;
-using Moq.AutoMock;
 
 namespace InMemoryLibraryTests
 {
-    public class Tests
-    {
-        [Test]
-        public async Task GetOrCreateAsync_DataChangesBetweenCalls_ShouldReturnFromCache()
-        {
-            // Arrange
-            var key = "testString";
-            var expectedValue = "testValue";
-            var isFactoryCalled = false;
-            var mocker = new AutoMocker(MockBehavior.Default, DefaultValue.Mock);
+	public class Tests
+	{
+		[Test]
+		public async Task GetOrCreateAsync_DataChangesBetweenCalls_ShouldReturnFromCache()
+		{
+			// Arrange
+			var key = "testString";
+			var expectedValue = "testValue";
+			var isFactoryCalled = false;
 
-            Func<Task<string>> factory = () =>
-            {
-                isFactoryCalled = true;
-                return Task.FromResult(isFactoryCalled ? expectedValue : string.Empty);
-            };
-            var memoryCache = mocker.CreateValidInstanceOfMemoryCache();
+			Func<Task<string>> factory = () =>
+			{
+				isFactoryCalled = true;
+				return Task.FromResult(isFactoryCalled ? expectedValue : string.Empty);
+			};
+			var memoryCache = TestExtensions.CreateValidInstanceOfMemoryCache();
 
-            // Act
-            var firstCall = await memoryCache.GetOrCreateAsync(key, factory);
-            var secondCall = await memoryCache.GetOrCreateAsync(key, factory);
+			// Act
+			var firstCall = await memoryCache.GetOrCreateAsync(key, factory);
+			var secondCall = await memoryCache.GetOrCreateAsync(key, factory);
 
-            // Assert
-            firstCall.Should().Be(expectedValue);
-            secondCall.Should().Be(expectedValue);
-        }
+			// Assert
+			firstCall.Should().Be(expectedValue);
+			secondCall.Should().Be(expectedValue);
+		}
 
-        [Test]
-        public async Task GetOrCreateAsync_AppSettingsNotValid_ShouldThrowNotImplementedException()
-        {
-            // Arrange
-            var key = "testKey";
-            var expectedValue = "testValue";
-            var mocker = new AutoMocker(MockBehavior.Default, DefaultValue.Mock);
+		[Test]
+		public async Task GetOrCreateAsync_AppSettingsNotValid_ShouldThrowNotImplementedException()
+		{
+			// Arrange
+			var key = "testKey";
+			var expectedValue = "testValue";
 
-            Func<Task<string>> factory = () => Task.FromResult(expectedValue);
+			Func<Task<string>> factory = () => Task.FromResult(expectedValue);
 
-            var memoryCache = mocker.CreateInvalidInstanceOfMemoryCache();
+			var memoryCache = TestExtensions.CreateInvalidInstanceOfMemoryCache();
 
-            // Act
-            Action action = () => memoryCache.GetOrCreateAsync(key, factory);
+			// Act
+			Action action = () => memoryCache.GetOrCreateAsync(key, factory);
 
-            // Assert
-            action.Should().Throw<NotImplementedException>();
-        }
+			// Assert
+			action.Should().Throw<NotImplementedException>();
+		}
 
-        [Test]
-        public async Task GetOrCreateAsync_DataStorageTimeNotSet_ShouldThrowNotImplementedException()
-        {
-            // Arrange
-            var key = "testInt";
-            var expectedValue = 1;
-            var mocker = new AutoMocker(MockBehavior.Default, DefaultValue.Mock);
+		[Test]
+		public async Task GetOrCreateAsync_DataStorageTimeNotSet_ShouldThrowNotImplementedException()
+		{
+			// Arrange
+			var key = "testInt";
+			var expectedValue = 1;
 
-            Func<Task<int>> factory = () => Task.FromResult(expectedValue);
+			Func<Task<int>> factory = () => Task.FromResult(expectedValue);
 
-            var memoryCache = mocker.CreateValidInstanceOfMemoryCache();
+			var memoryCache = TestExtensions.CreateValidInstanceOfMemoryCache();
 
-            // Act
-            Action action = () => memoryCache.GetOrCreateAsync(key, factory);
+			// Act
+			Action action = () => memoryCache.GetOrCreateAsync(key, factory);
 
-            // Assert
-            action.Should().Throw<NotImplementedException>();
-        }
+			// Assert
+			action.Should().Throw<NotImplementedException>();
+		}
 
-        [Test]
-        public async Task GetOrCreateAsync_DataChangesBetweenCalls_1()
-        {
-            // Arrange
-            var key = "testString1";
-            var expectedValue = "testValue";
-            var isFactoryCalled = false;
-            var mocker = new AutoMocker(MockBehavior.Default, DefaultValue.Mock);
+		[Test]
+		public void GetOrCreateAsync_DataChangesBetweenCalls_1()
+		{
+			// Arrange
+			var key = "testString1";
 
-            Func<Task<string>> factory = () =>
-            {
-                isFactoryCalled = true;
-                return Task.FromResult(isFactoryCalled ? expectedValue : string.Empty);
-            };
-            var memoryCache = mocker.CreateValidInstanceOfMemoryCache();
+			var memoryCacheMock = new Mock<IMemoryCache>();
+			memoryCacheMock.Setup(mc => mc.Remove(It.IsAny<object>())).Verifiable();
 
-            // Act
-            memoryCache.Remove(key);
+			var memoryCacheService = TestExtensions.CreateValidInstanceOfMemoryCache(memoryCacheMock);
 
-            // Assert
-            mocker.GetMock<IMemoryCache>().Verify(x => x.Remove(key), Times.Once);
-        }
-    }
+			// Act
+			memoryCacheService.Remove(key);
+
+			// Assert
+			memoryCacheMock.Verify(x => x.Remove(key), Times.Once);
+		}
+	}
 }
