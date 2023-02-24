b0b23b8bd981a1120d1a0747cf38cba5c4a4ef91
pavel haharyn <pavel.h@bluetape.com>
2023-02-24 16:44:05 +0300

    exclude code from coverage

--- InMemoryCachingLibrary/ServiceRegistry.cs
+++ InMemoryCachingLibrary/ServiceRegistry.cs
@@ -2,17 +2,19 @@
 using InMemoryCachingLibrary.Services.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
 
 namespace InMemoryCachingLibrary
 {
-    public static class ServiceRegistry
-    {
-        public static void AddInMemoryCachingService(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddMemoryCache();
-            services.Configure<CacheSettings>(configuration.GetSection(nameof(CacheSettings)));
-            services.AddSingleton<ICacheService, CacheService>();
+	[ExcludeFromCodeCoverage]
+	public static class ServiceRegistry
+	{
+		public static void AddInMemoryCachingService(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddMemoryCache();
+			services.Configure<CacheSettings>(configuration.GetSection(nameof(CacheSettings)));
+			services.AddSingleton<ICacheService, CacheService>();
 
-        }
-    }
+		}
+	}
 }
