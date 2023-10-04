5c4d02694dc14c16987d4e546d26ad71250cd5d6
Vadim Minutko <extvadim.minutko@7peakssoftware.com>
2023-10-04 09:33:08 +0200

    move to inmemory

--- BlazorServerTest.Core/ServiceCollectionRegistry.cs
+++ BlazorServerTest.Core/ServiceCollectionRegistry.cs
@@ -2,6 +2,7 @@
 using BlazorServerTest.Core.Business.AutoMapper;
 using BlazorServerTest.Core.Data;
 using BlazorServerTest.Core.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -11,8 +12,12 @@ public static class ServiceCollectionRegistry
     private const string MssqlConnectionString = "Server=localhost;Database=BlazorTest;Trusted_Connection=True;Encrypt=False;";
     public static void AddCoreServices(this IServiceCollection services)
     {
-        services.AddEntityFrameworkSqlite().AddDbContext<ApplicationDbContext>();
+        //services.AddEntityFrameworkSqlite().AddDbContext<ApplicationDbContext>();
         //services.AddDbContext<AppDbContext>(x => x.UseSqlServer(MssqlConnectionString));
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseInMemoryDatabase(databaseName: "InMemoryDb");
+        });
 
         services.AddAutoMapper(typeof(AutoMapperProfile).GetTypeInfo().Assembly);
 
