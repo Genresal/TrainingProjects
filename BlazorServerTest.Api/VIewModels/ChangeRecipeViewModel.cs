653918418743948d16d31d0079792113b15e75b5
Vadim Minutko <extvadim.minutko@7peakssoftware.com>
2023-10-05 10:22:05 +0200

    add vieew modal window

--- BlazorServerTest.Api/VIewModels/ChangeRecipeViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using BlazorServerTest.Core.Models.Categories;
-
-namespace BlazorServerTestApi.VIewModels;
-public class ChangeRecipeViewModel
-{
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public List<CategoryResponse>? Categories { get; set; }
-}
